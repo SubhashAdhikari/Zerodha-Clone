809cb578b32ea5af5dd6966cc3c980d3490221aa
Subhash Singh Adhikari <hi.iamsubhashadhikari@gmail.com>
2025-08-03 20:24:02 +0530

    Updated index.html

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 
-import HomePage from "./landing_page/home/Homepage";
+import HomePage from "./landing_page/home/HomePage";
 import Signup from "./landing_page/signup/Signup";
 import AboutPage from "./landing_page/about/AboutPage";
-import ProductPage from "./landing_page/products/ProductPage";
+import ProductPage from "./landing_page/products/ProductsPage";
 import PricingPage from "./landing_page/pricing/PricingPage";
 import SupportPage from "./landing_page/support/SupportPage";
 
@@ -17,7 +17,7 @@ import Footer from "./landing_page/Footer";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <Navbar />
+    
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/signup" element={<Signup />} />
@@ -27,6 +27,7 @@ root.render(
       <Route path="/support" element={<SupportPage />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
+    
     <Footer />
   </BrowserRouter>
 );
--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -3,23 +3,19 @@ import React from "react";
 function Hero() {
   return (
     <div className="container p-5 mb-5">
-      <div className="row text-center">
-        <img
-          src="media/images/homeHero.png"
-          alt="Hero Image"
-          className="mb-5"
-        />
-        <h1 className="mt-5">Invest in everything</h1>
-        <p>
-          Online platform to invest in stocks, derivatives, mutual funds, and
-          more
-        </p>
-        <button
-          className="p-2 btn btn-primary fs-5 mb-5"
-          style={{ width: "20%", margin: "0 auto" }}
-        >
-          Signup Now
-        </button>
+      <div className="row justify-content-center text-center">
+        <div className="col-md-8">
+          <img
+            src="media/images/homeHero.png"
+            alt="Hero"
+            className="img-fluid mb-4"
+          />
+          <h1 className="mt-4">Invest in everything</h1>
+          <p className="lead">
+            Online platform to invest in stocks, derivatives, mutual funds, and more
+          </p>
+          <button className="btn btn-primary btn-lg mt-3">Signup Now</button>
+        </div>
       </div>
     </div>
   );
